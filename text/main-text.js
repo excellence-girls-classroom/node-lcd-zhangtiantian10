24033e92f4df0114a917efc332ad22528c616831
zhangtiantian10 <975571814@qq.com>
2016-04-26 20:53:37 +0800

    add text at node.js

---
+++ text/main-text.js
@@ -0,0 +1,38 @@
+/**
+ * Created by zhangtian on 16-4-26.
+ */
+var main = require('../main/main.js');
+var fixtures = require('./fixtures.js');
+describe('unit text',function () {
+
+    var LCDNumber;
+    beforeEach(function () {
+       LCDNumber = fixtures.loadLCDNumber();
+    });
+    describe('splitNumber',function () {
+        it('correct',function () {
+            var number = main.splitNumber(910);
+            expect(number).toEqual([9,1,0]);
+        });
+    });
+
+    describe('searchSameNumber',function () {
+
+        it('correct',function () {
+           expect(main.searchSameNumber([9,1,0],LCDNumber)).toEqual(['._. ... ._. ','|_| ..| |.| ','..| ..| |_| ']);
+        });
+    });
+
+    describe('print',function () {
+
+        it('correct',function () {
+
+            spyOn(console, 'log');
+            main.createLCDString(910);
+            var printLCD = '._. ... ._. \n'+
+                '|_| ..| |.| \n'+
+                '..| ..| |_| \n';
+            expect(console.log).toHaveBeenCalledWith(printLCD);
+        });
+    })
+});
