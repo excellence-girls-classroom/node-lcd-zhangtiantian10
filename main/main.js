86eceee2d043520787bc396fc6ac6df11b336a4d
zhangtiantian10 <975571814@qq.com>
2016-05-03 21:35:01 +0800

    modify funtion name and variable name

--- main/main.js
+++ main/main.js
@@ -1,15 +1,12 @@
-var LCDNumber = require('../text/fixtures.js');
+var LcdNumber = require('../text/fixtures.js');
 function createLCDString(input) {
-    var LcdNumber = LCDNumber.loadLCDNumber();
-    var numberArray = splitNumber(input);
-    var LCDArray = searchSameNumber(numberArray,LcdNumber);
-    print(LCDArray);
-
+    var lcdLines = generateLCDLines(splitInput(input),LcdNumber.loadLcdNumberDict());
+    var lcdText = generateLCDText(lcdLines);
+    console.log(lcdText);
 }
 
-function splitNumber(input) {
-    input = input + '';
-    var numberArray = input.split('');
+function splitInput(input) {
+    var numberArray = input.toString().split('');
     for(var i = 0; i<numberArray.length;i++){
         numberArray[i] = parseInt(numberArray[i]);
     }
@@ -17,30 +14,33 @@ function splitNumber(input) {
     return numberArray;
 }
 
-function searchSameNumber(numberArray, LcdNumber) {
-    var LCDArray = [];
-    for(var j = 0; j<LcdNumber.length;j++) {
-        LCDArray[j] = '';
-        for (var i = 0; i < numberArray.length; i++) {
-            LCDArray[j] += LcdNumber[j][numberArray[i]];
-        }
+function generateLCDLine(numberArray, LCDNumber) {
+    var lcdLine = '';
+    for (var i = 0; i < numberArray.length; i++) {
+        lcdLine += LCDNumber[numberArray[i]];
     }
-    
-    return LCDArray;
+    return lcdLine;
+};
+
+function generateLCDLines(numberArray, lcdNumberDict) {
+    var lcdLines = [];
+    for (var i = 0; i < lcdNumberDict.length; i++) {
+        lcdLines.push(generateLCDLine(numberArray, lcdNumberDict[i]));
+    }
+    return lcdLines;
 }
 
-function print(LCDArray) {
-    var printLCD = '';
-    for(var i = 0; i<LCDArray.length;i++){
-        printLCD += (LCDArray[i]+'\n');
+function generateLCDText(lcdLines) {
+    var lcdText = '';
+    for(var i = 0; i<lcdLines.length;i++){
+        lcdText += (lcdLines[i]+'\n');
     }
-    
-    console.log(printLCD);
+    return lcdText;
 }
 
 module.exports = {
     createLCDString: createLCDString,
-    splitNumber: splitNumber,
-    searchSameNumber: searchSameNumber,
-    print: print
+    splitInput: splitInput,
+    generateLCDLines: generateLCDLines,
+    generateLCDText: generateLCDText
 }
